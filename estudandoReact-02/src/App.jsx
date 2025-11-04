0d0abcdc84915f43ba7034006cdb474efa596174
Felipe Freires <felipeplayf13@gmail.com>
2025-11-04 11:51:00 -0300

    primeiro commits das atividades de react

---
+++ estudandoReact-02/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const estiloS = {
+  color: 'green'
+}
+
+const App = () => {
+  const dados = mario;
+
+const total = dados.compras
+  .map(item => Number(item.preco.replace('R$ ', '')))
+  .reduce((acc, itemAtual) => acc + itemAtual)
+  return(
+      <div>
+    <p>Nome: {dados.cliente}</p>
+    <p>Idade: {dados.idade}</p>
+    <p>Situaçao: <span style={{color: dados.ativa ? 'green' : 'red'}}>{dados.ativa ? 'ativo' : 'inativo'}</span> </p>
+    <p>Total gasto: {total}
+    </p>
+      {total > 10000 && <p>Voce esta gastando muito</p> }
+  </div>
+  )
+};
+
+export default App
